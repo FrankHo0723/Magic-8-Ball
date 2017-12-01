57fd00f4b969a5d8667bbaa8acf223c20c5791bf
Your Name <you@example.com>
2017-12-02 01:15:13 +0800

    first commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomNumber : Int = 0
+    let imageArray = ["ball1","ball2","ball3","ball4","ball5"]
 
+    @IBOutlet weak var ballImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBallImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    @IBAction func askButton(_ sender: UIButton) {
+        updateBallImage()
+    }
+    func updateBallImage (){
+        randomNumber = Int(arc4random_uniform(5))
+        ballImage.image = UIImage(named:imageArray[randomNumber])
     }
 
 
